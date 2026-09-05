2db2d54ec8ba70bfab289e01060a3925d35393b5
weikang01-10O1 <weihua01@ucm.es>
2026-09-05 02:56:51 +0200

    Add files via upload

---
+++ sensor_ultrasonic.ino
@@ -0,0 +1,157 @@
+/* Heltec Automation LoRaWAN communication example
+ *
+ * Function:
+ * 1. Upload node data to the server using the standard LoRaWAN protocol.
+ *  
+ * Description:
+ * 1. Communicate using LoRaWAN protocol.
+ * 
+ * HelTec AutoMation, Chengdu, China
+ * 成都惠利特自动化科技有限公司
+ * www.heltec.org
+ *
+ * */
+
+#include "LoRaWan_APP.h"
+
+/* OTAA para LoRa-01 */
+uint8_t devEui[] = {  };
+uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appKey[] = {  };
+
+/* Licencia del Heltec WiFi LoRa 32 V3*/
+uint32_t license[4] = {};
+
+/* ABP para*/
+uint8_t nwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda,0x85 };
+uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
+uint32_t devAddr =  ( uint32_t )0x007e6ae1;
+
+/*LoraWan channelsmask, default channels 0-7*/ 
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms]. Cada 15 s el periodo de transmision*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable
+Adaptive Data Rate (ADR) was enabled to allow the LoRaWAN network server to 
+automatically select the most appropriate spreading factor according to the radio link conditions.*/
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+// HC-SR04 Pin
+#define TRIG_PIN 41
+#define ECHO_PIN 42
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
+{
+    // Medicion
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    
+    long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+    float distance_cm = duration * 0.034 / 2;
+    if (distance_cm > 400) distance_cm = 400;
+    if (distance_cm < 2) distance_cm = 2;
+    
+    uint16_t dist = (uint16_t)distance_cm;
+    appDataSize = 2;
+    appData[0] = dist >> 8;
+    appData[1] = dist & 0xFF;
+}
+
+//if true, next uplink will add MOTE_MAC_DEVICE_TIME_REQ 
+
+
+void setup() {
+  Serial.begin(115200);
+  // Configurar licencia uint32_t license[4] = {0xC9FC7C50,0xFCB13912,0x6D73C14D,0xD52443B3};
+  Mcu.setlicense(license, HELTEC_BOARD);
+  Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+}
+
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+#if(LORAWAN_DEVEUI_AUTO)
+      LoRaWAN.generateDeveuiByChipID();
+#endif
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      //both set join DR and DR when ADR off 
+      LoRaWAN.setDefaultDR(3);
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      prepareTxFrame( appPort );
+      LoRaWAN.send();
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.sleep(loraWanClass);
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
+}
